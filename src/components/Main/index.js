4091403ed4fc85d2738142144d8276ed327baca8
PinkAttitude77 <betsy.petering@sbcglobal.net>
2020-06-14 13:57:11 -0700

    initial commit

---
+++ src/components/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Main() {
+  return (
+    <div>
+      <h2>Find Books Using Google</h2>
+      <h3>Search for Books, Make a Wish List</h3>
+    </div>
+  );
+}
+
+export default Main;
